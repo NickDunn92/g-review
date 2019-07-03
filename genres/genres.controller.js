342efeef6618ba3e6059e174ec6f0b58e6128cf1
Nick Dunn <nickdunn92@icloud.com>
2019-07-03 20:24:09 +0100

    Added get all and create routes

---
+++ genres/genres.controller.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const { getGenres, addGenre } = require('./genres.service');
+
+router.get('/', async (req, res) => {
+    const genres = await getGenres();
+    res.send(genres);
+});
+
+router.post('/', async (req, res) => {
+  const { name } = req.body;
+  const genre = await addGenre(name);
+  res.send(genre);
+});
+
+module.exports = router;
