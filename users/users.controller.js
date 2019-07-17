d83ceaaf04b58970f14cc75551bf859627a845e9
Nick Dunn <nickdunn92@icloud.com>
2019-07-17 06:33:43 +0100

    added age property to user model

--- users/users.controller.js
+++ users/users.controller.js
@@ -1,22 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { getUser, getUsers, addUser } = require('./users.service');
+const { getUser, getUsers, addUser } = require("./users.service");
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   const users = await getUsers();
   res.send(users);
 });
 
-router.post('/', async (req, res) => {
-  const { username, email, firstname, lastname } = req.body;
-  const user = await addUser(username, email, firstname, lastname);
+router.post("/", async (req, res) => {
+  const { username, email, firstname, lastname, age } = req.body;
+  const user = await addUser(username, email, firstname, lastname, age);
   res.send(user);
 });
 
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   const user = await getUser(req.params.id);
 
-  if(!user) return res.status(404).send('The user with the given ID was not found');
+  if (!user)
+    return res.status(404).send("The user with the given ID was not found");
 
   res.send(user);
 });
--- users/users.service.js
+++ users/users.service.js
@@ -1,24 +1,24 @@
-const { User } = require('./users.schema');
+const { User } = require("./users.schema");
 
 exports.getUsers = async () => {
   const users = await User.find();
   return users;
-}
+};
 
-exports.addUser = async (username, email, firstname, lastname) => {
+exports.addUser = async (username, email, firstname, lastname, age) => {
   const user = new User({
     username,
     email,
     firstname,
-    lastname
+    lastname,
+    age
   });
 
   await user.save();
   return user;
-}
+};
 
-exports.getUser = async (id) => {
+exports.getUser = async id => {
   const user = await User.findById(id);
   return user;
-}
-
+};
