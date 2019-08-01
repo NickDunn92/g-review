4a9a489d3677849aee34667a5ca99fa99e1d2b9c
Nick Dunn <nickdunn92@icloud.com>
2019-08-01 19:31:30 +0100

    Added try catch blocks and corrected code

--- games/games.controller.js
+++ games/games.controller.js
@@ -14,57 +14,76 @@ router.get("/", async (req, res) => {
   res.send(games);
 });
 
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const game = await getGame(id);
+
+    if (!game)
+      return res.status(404).send("The movie with the given ID was not found.");
+
+    res.send(game);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
 router.post("/", async (req, res) => {
-  const { name, genres, description, ageRating } = req.body;
+  try {
+    const { name, genres, description, ageRating } = req.body;
 
-  const genreObjects = [];
+    const genreObjects = [];
 
-  genres.map(async g => {
-    let genre = await getGenre(g);
-    genreObjects.push(genre);
-  });
+    genres.map(async g => {
+      let genre = await getGenre(g);
+      genreObjects.push(genre);
+    });
 
-  const game = await addGame(name, genreObjects, description, ageRating);
+    const game = await addGame(name, genreObjects, description, ageRating);
 
-  res.send(game);
+    res.send(game);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
 router.put("/:id", async (req, res) => {
-  const { name, genres, description, ageRating } = req.body;
+  try {
+    const { id } = req.params;
+    const { name, genres, description, ageRating } = req.body;
 
-  const genreObjects = [];
+    const genreObjects = [];
 
-  genres.map(async g => {
-    let genre = await getGenre(g);
-    genreObjects.push(genre);
-  });
+    genres.map(async g => {
+      let genre = await getGenre(g);
+      genreObjects.push(genre);
+    });
 
-  const game = await updateGame(req.params.id, {
-    name,
-    genres: genreObjects,
-    description,
-    ageRating
-  });
+    const game = await updateGame(id, {
+      name,
+      genres: genreObjects,
+      description,
+      ageRating
+    });
 
-  if (!game)
-    return res.status(404).send("The game with the given ID was not found.");
+    if (!game)
+      return res.status(404).send("The game with the given ID was not found.");
 
-  res.send(game);
+    res.send(game);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
 router.delete("/:id", async (req, res) => {
-  await removeGame(req.params.id);
+  try {
+    const { id } = req.params;
+    await removeGame(id);
 
-  res.status(200).send();
-});
-
-router.get("/:id", async (req, res) => {
-  const game = await getGame(req.params.id);
-
-  if (!game)
-    return res.status(404).send("The movie with the given ID was not found.");
-
-  res.send(game);
+    res.status(200).send();
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
 module.exports = router;
